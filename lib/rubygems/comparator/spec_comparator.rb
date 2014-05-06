7c431014688759747ed878d09fc3f60ec405ce0b
Josef Stribny <jstribny@redhat.com>
2014-05-06 13:35:26 +0200

    Refactor spec comparison

--- lib/rubygems/comparator/spec_comparator.rb
+++ lib/rubygems/comparator/spec_comparator.rb
@@ -11,27 +11,39 @@ class Gem::Comparator
       info 'Checking spec parameters...'
 
       filter_params(SPEC_PARAMS, options[:param]).each do |param|
-        values = []
-        specs.each do |s|
-          if s.respond_to? :"#{param}"
-            values << s.send(:"#{param}")
-          else
-            warn "#{s.full_name} does not respond to #{param}, skipping check"
-          end
-        end
+        values = values_from_specs(param, specs)
 
         # Are values the same?
-        if values.count(values[0]) == values.size
+        if same_values? values
           report[param] << "#{SUCCESS} #{param}" if options[:log_all]
         else
           report[param].set_header "#{FAIL} #{param}:"
           values.each_with_index do |value, index|
-            report[param] << "#{Rainbow(specs[index].version).blue}: #{value}"
+            report[param] << \
+              "#{Rainbow(specs[index].version).blue}: #{value}"
           end
         end
       end
       report
     end
 
+    private
+
+      def values_from_specs(param, specs)
+	values = []
+        specs.each do |s|
+          if s.respond_to? :"#{param}"
+            values << s.send(:"#{param}")
+          else
+            warn "#{s.full_name} does not respond to " +
+		 "#{param}, skipping check"
+          end
+        end
+	values
+      end
+
+      def same_values?(values)
+        values.count(values[0]) == values.size
+      end
   end
 end
