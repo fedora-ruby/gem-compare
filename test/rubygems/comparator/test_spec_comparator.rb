60df8276478ddae7c8ddc3821c625f41b0294546
Josef Strzibny <strzibny@strzibny.name>
2014-06-21 13:31:29 +0200

    Test with log_all option

--- test/rubygems/comparator/test_spec_comparator.rb
+++ test/rubygems/comparator/test_spec_comparator.rb
@@ -5,7 +5,7 @@ class TestSpecComparator < Gem::TestCase
   def setup
     super
 
-    options = { keep_all: true, no_color: true }
+    options = { keep_all: true, log_all: true, no_color: true }
     versions = ['0.0.1', '0.0.2', '0.0.3', '0.0.4']
 
     @comparator = Gem::Comparator.new(options)
