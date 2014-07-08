68fd31180a2a07d939c579474d1d99a4312bca05
Josef Stribny <jstribny@redhat.com>
2014-07-08 12:39:22 +0200

    Test comparison of development dependencies

--- test/rubygems/comparator/test_dependency_comparator.rb
+++ test/rubygems/comparator/test_dependency_comparator.rb
@@ -33,4 +33,13 @@ class TestDependencyComparator < Gem::TestCase
     assert_equal [], @report['runtime_dependency']['0.0.3->0.0.4']['deleted'].messages
     assert_equal ['rails from: [">= 4.0.0"] to: [">= 4.1.0"]'], @report['runtime_dependency']['0.0.3->0.0.4']['updated'].lines(1)
   end
+
+  def test_development_dependencies_comparison
+    assert_equal [], @report['development_dependency']['0.0.1->0.0.2']['added'].messages
+    assert_equal [], @report['development_dependency']['0.0.1->0.0.2']['deleted'].messages
+    assert_equal [], @report['development_dependency']['0.0.1->0.0.2']['updated'].messages
+    assert_equal [], @report['development_dependency']['0.0.2->0.0.3']['added'].messages
+    assert_equal [], @report['development_dependency']['0.0.2->0.0.3']['deleted'].messages
+    assert_equal [], @report['development_dependency']['0.0.2->0.0.3']['updated'].messages
+  end
 end
