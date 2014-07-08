17b71f9416b286f838683696985104a0505a4e66
Josef Stribny <jstribny@redhat.com>
2014-07-08 13:58:50 +0200

    Test file lists comparison

---
+++ test/rubygems/comparator/test_file_list_comparator.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class TestFileListComparator < TestGemComparator
+
+  def test_files_comparison
+    assert_equal 'DIFFERENT files:', @report['files'].header.data
+    assert_equal '0.0.1->0.0.2:', @report['files'].lines(1)
+    assert_equal ["CHANGELOG.md"], @report['files']['0.0.1->0.0.2']['added'].lines(1)
+    assert_equal [], @report['files']['0.0.1->0.0.2']['deleted'].messages
+    assert_equal [], @report['files']['0.0.1->0.0.2']['updated'].messages
+    assert_equal ["bin/lorem", "lib/lorem/utils.rb"], @report['files']['0.0.2->0.0.3']['added'].lines(1)
+    assert_equal [], @report['files']['0.0.2->0.0.3']['deleted'].messages
+    assert_equal [], @report['files']['0.0.2->0.0.3']['updated'].messages
+    assert_equal [], @report['files']['0.0.3->0.0.4']['added'].messages
+    assert_equal [], @report['files']['0.0.3->0.0.4']['deleted'].messages
+    assert_equal [], @report['files']['0.0.3->0.0.4']['updated'].messages
+  end
+
+  def test_test_files_comparison
+    assert_equal 'SAME test_files', @report['test_files'].header.data
+  end
+end
