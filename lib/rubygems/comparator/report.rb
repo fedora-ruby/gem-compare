5b52c093b34db6ffd0ef7eb4f8cfce9a6eabf472
Josef Stribny <jstribny@redhat.com>
2014-07-31 13:58:55 +0200

    Move Entry to a new file

--- lib/rubygems/comparator/report.rb
+++ lib/rubygems/comparator/report.rb
@@ -1,3 +1,5 @@
+require 'rubygems/comparator/report/entry'
+
 class Gem::Comparator
 
   ##
@@ -123,41 +125,6 @@ class Gem::Comparator
           parent_section = parent
         end
 
-    end
-
-    class Entry
-      include Gem::UserInteraction
-
-      attr_accessor :data, :indent
-
-      def initialize(data = '', indent = '')
-        @data = data
-        @indent = indent
-      end
-
-      def set_indent!(indent)
-        @indent = indent
-        self
-      end
-
-      def empty?
-        case @data
-        when String, Array
-          @data.empty?
-        end
-      end
-
-      def print
-        printed = case @data
-                  when String
-                    "#{@indent}#{@data}"
-                  when Array
-                    @indent + @data.join("\n#{@indent}")
-                  else
-                    "#{@indent}#{@data}"
-                  end
-        say printed
-      end
     end
   end
 end
