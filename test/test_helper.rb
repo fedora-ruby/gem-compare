2f1d28d6c563fa934bd612cbeed7e2f42c997ebd
Patrik Ragnarsson <patrik@starkast.net>
2022-07-03 16:23:50 +0200

    Adjust file permissions before running tests
    Works without these chmod's if you have umask 002 when cloning the repo.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,21 @@
 require 'minitest/autorun'
 require 'rubygems/comparator'
 
+# The tests expects these permissions, which isn't the case if you have
+# umask 022 when cloning the repo
+def setup_file_permissions
+  gemfiles_path = File.expand_path('gemfiles', File.dirname(__FILE__))
+  File.chmod(0664, File.join(gemfiles_path, 'lorem-0.0.1', 'lib', 'lorem.rb'))
+  File.chmod(0664, File.join(gemfiles_path, 'lorem-0.0.3', 'bin', 'lorem'))
+  File.chmod(0664, File.join(gemfiles_path, 'lorem-0.0.4', 'lib', 'lorem.rb'))
+  File.chmod(0775, File.join(gemfiles_path, 'lorem-0.0.4', 'bin', 'lorem'))
+end
+
 class TestGemComparator < Minitest::Test
   def setup
     super
+
+    setup_file_permissions
 
     options = { keep_all: true, log_all: true, no_color: true, brief_mode: false }
     versions = ['0.0.1', '0.0.2', '0.0.3', '0.0.4']
@@ -22,6 +34,7 @@ end
 class TestGemModule < Minitest::Test
   def setup
     super
+    setup_file_permissions
     gemfiles_path = File.expand_path('gemfiles', File.dirname(__FILE__))
     @v001 = File.join(gemfiles_path, 'lorem-0.0.1')
     @v002 = File.join(gemfiles_path, 'lorem-0.0.2')
